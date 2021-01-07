7fb007cf48f764cfdfac863fe9b13d9ae93588c7
Sahithi Bellam <sahithibellam@gmail.com>
2021-01-07 22:17:47 +0530

    added hello sahithi

--- PractiseApp/src/com/lti/sample/SampleClass.java
+++ PractiseApp/src/com/lti/sample/SampleClass.java
@@ -3,7 +3,7 @@ package com.lti.sample;
 public class SampleClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hello Sahithi");
 		System.out.println("Hi Sahithi");
 
 		System.out.println("Hi");
