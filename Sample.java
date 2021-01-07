aed1caef20fde746508faa59040dc60ace0b91ae
Sahithi-bellam <76149001+Sahithi-bellam@users.noreply.github.com>
2021-01-07 23:47:47 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -2,7 +2,7 @@ public class Sample{
 
 	public static void main(String[] args) {
 		System.out.println("Hello Sahithi");
-		System.out.println("Hi Sahithi");
+		System.out.println("Hi ");
 
 		System.out.println("Hi Sahithi");
 
