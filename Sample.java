2c59722e9b244554ef9e647e61e07ce391402b62
Sahithi-bellam <76149001+Sahithi-bellam@users.noreply.github.com>
2021-01-07 23:43:36 +0530

    added sample

---
+++ Sample.java
@@ -0,0 +1,11 @@
+public class Sample{
+
+	public static void main(String[] args) {
+		System.out.println("Hello Sahithi");
+		System.out.println("Hi Sahithi");
+
+		System.out.println("Hi");
+
+	}
+
+}
